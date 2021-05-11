41ad64a90fc7d4d26f9ae8661fb28b809502559f
Matthew Seat <matts0613@users.noreply.github.com>
2021-05-11 13:09:38 -0400

    Update Googler.js

--- Googler.js
+++ Googler.js
@@ -1,1 +1,8 @@
 javascript:(function()%7B(function google() %7B%0A    var search%3D prompt("Please enter your search terms:")%3B%0A    window.open("https%3A%2F%2Fwww.google.com%2Fsearch%3Fq%3D" %2B search)%3B%0A%7D)()%7D)()%3B
+
+// (function () {
+//  (function google() {
+//      var search = prompt("Please enter your search terms:");
+//      window.open("https://www.google.com/search?q=" + search);
+//  })()
+//})();
